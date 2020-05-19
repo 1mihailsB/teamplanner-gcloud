958fa797b785a6715b6f7e7e81505609a36da695
Mihails B <1mihails.b@gmail.com>
2020-05-19 14:12:44 +0200

    change sql script for gcloud

--- mysql-schema.sql
+++ mysql-schema.sql
@@ -1,14 +1,9 @@
 use website_rest;
-ALTER DATABASE `website_rest` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 
 SET FOREIGN_KEY_CHECKS = 0;
 
-drop table if exists users;
-drop table if exists gameplans;
-drop table if exists friendships;
-drop table if exists gameplan_members;
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
   `googlesub` varchar(255) UNIQUE NOT NULL,
   `name` varchar (50) NOT NULL,
   `email` varchar (50) NOT NULL,
@@ -21,7 +16,7 @@ CREATE TABLE users (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE utf8_bin;
 
 
-CREATE TABLE gameplans (
+CREATE TABLE IF NOT EXISTS gameplans (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
     `title` varchar(50) NOT NULL,
     `main_text` varchar(3000),
@@ -33,7 +28,7 @@ CREATE TABLE gameplans (
     CONSTRAINT `UQ_author_title_` UNIQUE(`author_googlesub`, `title`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE utf8_bin;
 
-CREATE TABLE friendships (
+CREATE TABLE IF NOT EXISTS friendships (
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `friend_1_googlesub` varchar(255),
 `friend_2_googlesub` varchar(255),
@@ -53,8 +48,10 @@ CONSTRAINT `FK_friend_2` FOREIGN KEY (`friend_2_googlesub`)
 REFERENCES `users` (`googlesub`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE utf8_bin;
 
+
+DROP TRIGGER IF EXISTS quefriendships;
 delimiter $
-create trigger uniquefriendships before insert on friendships
+create trigger quefriendships before insert on friendships
 for each row
 begin
   if exists (
@@ -68,7 +65,7 @@ begin
 end$
 delimiter ;
 
-CREATE TABLE gameplan_members (
+CREATE TABLE IF NOT EXISTS gameplan_members (
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `gameplan_id` int(11) NOT NULL,
 `member_user_googlesub` varchar(255),
