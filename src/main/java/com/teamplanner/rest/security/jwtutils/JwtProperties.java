37a967c0e7f5d569564883ef2406aec87fb38006
Mihails B <1mihails.b@gmail.com>
2020-05-23 14:01:10 +0200

    change cookie expiration

--- src/main/java/com/teamplanner/rest/security/jwtutils/JwtProperties.java
+++ src/main/java/com/teamplanner/rest/security/jwtutils/JwtProperties.java
@@ -2,7 +2,7 @@ package com.teamplanner.rest.security.jwtutils;
 
 public class JwtProperties {
 	public static final String SECRET ="development";
-	public static final int EXPIRATION_TIME_MILLISECONDS = 60*60*1000; //60 minutes in milliseconds
+	public static final int EXPIRATION_TIME_MILLISECONDS = 24*60*60*1000; //60 minutes in milliseconds
 	public static final String TOKEN_PREFIX = "Bearer:";
 	public static final String COOKIE_NAME = "Authorization";
 	
