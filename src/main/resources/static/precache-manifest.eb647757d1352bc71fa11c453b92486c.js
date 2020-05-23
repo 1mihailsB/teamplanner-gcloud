ddd777b91b343c6c474a9fedb2f1dc90b37ba79d
Mihails B <1mihails.b@gmail.com>
2020-05-24 00:42:16 +0200

    last react build was incomplete

---
+++ src/main/resources/static/precache-manifest.eb647757d1352bc71fa11c453b92486c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf789b7427e5db5ae86c65015794ebf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3692289b019187d472c8",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "7740afe5c847ea66c78e",
+    "url": "/static/css/main.b8170e7b.chunk.css"
+  },
+  {
+    "revision": "3692289b019187d472c8",
+    "url": "/static/js/2.adee3412.chunk.js"
+  },
+  {
+    "revision": "0d738ac4708c14c861847379e443bc09",
+    "url": "/static/js/2.adee3412.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7740afe5c847ea66c78e",
+    "url": "/static/js/main.04407ca5.chunk.js"
+  },
+  {
+    "revision": "cb15e4e537d28d8f68d5",
+    "url": "/static/js/runtime-main.52589c09.js"
+  },
+  {
+    "revision": "48d82d68a80d6376152d5f65efcde8d5",
+    "url": "/static/media/team.48d82d68.png"
+  }
+]);
