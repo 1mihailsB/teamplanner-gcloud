f645d78850b2d5c8ff7ab5b13d5a38a89d9c73d4
Mihails B <1mihails.b@gmail.com>
2020-05-24 01:57:39 +0200

    point frontend routs to same index.html of the react app

--- src/main/java/com/teamplanner/rest/controller/FrontendController.java
+++ src/main/java/com/teamplanner/rest/controller/FrontendController.java
@@ -12,26 +12,8 @@ import java.io.IOException;
 @Controller
 public class FrontendController {
 
-    @GetMapping("/")
-    public void noroute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        request.getRequestDispatcher("index.html").forward(request, response);
-    }
-
-    @GetMapping("/games")
-    public void games(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        request.getRequestDispatcher("index.html").forward(request, response);
-    }
-
-    @GetMapping("/friends")
-    public void friends(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        request.getRequestDispatcher("index.html").forward(request, response);
-    }
-
-    @GetMapping("/chooseNickname")
-    public void chooseNickname(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @GetMapping({"/", "/games", "/friends", "/chooseNickname"})
+    public void frontendRoutes(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         request.getRequestDispatcher("index.html").forward(request, response);
     }
