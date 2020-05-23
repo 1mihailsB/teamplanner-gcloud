a524681084daa723258cda7f943e22397703168f
Mihails B <1mihails.b@gmail.com>
2020-05-23 19:50:35 +0200

    try showing path to index.html

--- src/main/java/com/teamplanner/rest/controller/FrontendController.java
+++ src/main/java/com/teamplanner/rest/controller/FrontendController.java
@@ -5,12 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class FrontendController {
 
     @GetMapping("/")
     public String index(){
-        System.out.println("inside frontend controller");
-         return "index";
+         System.out.println("inside frontend controller");
+         return "/resources/index";
     }
 }
