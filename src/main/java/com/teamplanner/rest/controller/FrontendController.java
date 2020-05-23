12a6c85003c7e030f5c513ea1c2c223808b4610b
Mihails B <1mihails.b@gmail.com>
2020-05-23 19:33:16 +0200

    add front end files to static folder
    attempt to serve react app from this spring app

---
+++ src/main/java/com/teamplanner/rest/controller/FrontendController.java
@@ -0,0 +1,14 @@
+package com.teamplanner.rest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontendController {
+
+    @GetMapping("/")
+    public String index(){
+
+    return "index";
+    }
+}
