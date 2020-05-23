531fb5cd7a36a35724824278b79230e5020da972
Mihails B <1mihails.b@gmail.com>
2020-05-24 00:38:42 +0200

    delete nickname cookie on logout in front end too

--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8c09dd72e5b08eb5d38c2f09744f1bce.js"
+  "/precache-manifest.eb647757d1352bc71fa11c453b92486c.js"
 );
 
 self.addEventListener('message', (event) => {
