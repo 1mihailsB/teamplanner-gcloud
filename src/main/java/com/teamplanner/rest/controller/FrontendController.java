15f224bb5cff0123f39bf2eba8fc7a8e7292ebbb
Mihails B <1mihails.b@gmail.com>
2020-05-24 00:27:42 +0200

    redirect to react main page regadless of url

--- src/main/java/com/teamplanner/rest/controller/FrontendController.java
+++ src/main/java/com/teamplanner/rest/controller/FrontendController.java
@@ -4,12 +4,36 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @Controller
 public class FrontendController {
 
-    @GetMapping("/index")
-    public String index(){
-         System.out.println("inside frontend controller");
-         return "index";
+    @GetMapping("/")
+    public void noroute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.getRequestDispatcher("index.html").forward(request, response);
     }
+
+    @GetMapping("/games")
+    public void games(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.getRequestDispatcher("index.html").forward(request, response);
+    }
+
+    @GetMapping("/friends")
+    public void friends(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.getRequestDispatcher("index.html").forward(request, response);
+    }
+
+    @GetMapping("/chooseNickname")
+    public void chooseNickname(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.getRequestDispatcher("index.html").forward(request, response);
+    }
+
 }
