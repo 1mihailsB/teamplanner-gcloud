6cb56b17e2e28a0adf5edb58269b27a9795beead
Mihails B <1mihails.b@gmail.com>
2020-05-24 00:18:55 +0200

    add react app to static files

---
+++ src/main/resources/static/precache-manifest.ef1076d2a5e74c327ecfa269740f0689.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58de1ceb8db99c6984f37fedde5fed28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3692289b019187d472c8",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "163ac7c8b9b3fcae8cb1",
+    "url": "/static/css/main.b8170e7b.chunk.css"
+  },
+  {
+    "revision": "3692289b019187d472c8",
+    "url": "/static/js/2.adee3412.chunk.js"
+  },
+  {
+    "revision": "0d738ac4708c14c861847379e443bc09",
+    "url": "/static/js/2.adee3412.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "163ac7c8b9b3fcae8cb1",
+    "url": "/static/js/main.7ecccd5c.chunk.js"
+  },
+  {
+    "revision": "cb15e4e537d28d8f68d5",
+    "url": "/static/js/runtime-main.52589c09.js"
+  },
+  {
+    "revision": "48d82d68a80d6376152d5f65efcde8d5",
+    "url": "/static/media/team.48d82d68.png"
+  }
+]);
