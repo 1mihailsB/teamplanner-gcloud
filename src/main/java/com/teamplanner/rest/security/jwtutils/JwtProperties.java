4f53fa15e6357381110e791d4613fe6a699575c8
Mihail <1mihails.b@gmail.com>
2020-05-27 19:34:56 +0000

    fix forwarding&allowed paths. remove prop file and jwt secret from git

--- src/main/java/com/teamplanner/rest/security/jwtutils/JwtProperties.java
+++
@@ -1,9 +0,0 @@
-package com.teamplanner.rest.security.jwtutils;
-
-public class JwtProperties {
-	public static final String SECRET ="development";
-	public static final int EXPIRATION_TIME_MILLISECONDS = 10*24*60*60*1000; //10 days in milliseconds
-	public static final String TOKEN_PREFIX = "Bearer:";
-	public static final String COOKIE_NAME = "Authorization";
-	
-}
